dc8bb9c562f3e595a51c038d820b41728023da35
dbsq102 <dexterboh2002@gmail.com>
2022-09-18 18:13:57 +0800

    Login/Register
    Login/Register features included

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Welcome to the Quiz Website!') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    Still a work in progress...
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
